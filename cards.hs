1c5bdb4e634c2c34a8fc4aa71508483918e1116a
Andrew Shaw <andrew.t.shaw@gmail.com>
2014-10-19 02:53:46 -0700

    Card reading

--- cards.hs
+++ cards.hs
@@ -2,6 +2,8 @@ import qualified Data.Map as Map
 import Control.Applicative
 import qualified Data.List as List
 import Data.Function (on)
+import qualified Data.Set as Set
+import qualified Data.List.Split as Split
 
 data Value = Two | Three | Four | Five | Six | Seven | 
              Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord, Show, Read)
@@ -36,13 +38,39 @@ data Rank = High | Pair | TwoPair | ThreeKind | Straight |
 
 type Hand = [Card]
 
-type CardRank = (Rank, Card)
+type CardRank = (Rank, Hand)
 
 -- Hand, Rank, High card
 type BestHand = (Hand, CardRank, Card)
 
+readHandStr :: String -> Hand
+readHandStr str = map (readCardStr) cardSplit
+    where cardSplit = Split.splitOn " " str
+
 -- readHand :: Hand -> BestHand
 -- readHand Hand
+
+
+
+
+cardRank :: Hand -> CardRank
+cardRank hand = cardRank' hand (rankFunctionList hand)
+
+cardRank' :: Hand -> [(Rank, Maybe Hand)] -> CardRank
+cardRank' hand [] = (High, [highCard hand])
+cardRank' hand ((_, Nothing):xs) = cardRank' hand xs
+cardRank' hand ((rank, Just h):xs) = (rank, h)
+
+rankFunctionList :: Hand -> [(Rank, Maybe Hand)]
+rankFunctionList hand = map (mapHand hand) [(StraightFlush, isStraightFlush), (FourKind, isFourKind), 
+                                            (FullHouse, isFullHouse), (Flush, isFlush), (Straight, isStraight), 
+                                            (ThreeKind, isThreeKind), (TwoPair, isTwoPair), (Pair, isPair)]
+    where mapHand hand (rank, func) = (rank, func hand)
+
+
+
+
+
 
 sortHand :: Hand -> Hand
 sortHand hand = List.sortBy (compare `on` fst) hand
@@ -65,13 +93,6 @@ isFlush hand
 allTheSame :: (Eq a) => [a] -> Bool
 allTheSame xs = and $ map (== head xs) (tail xs)
 
-firstDuplicate :: (Eq a) => [a] -> [a]
-firstDuplicate [] = []
-firstDuplicate (x:[]) = [x]
-firstDuplicate (x:y:xs) 
-    | x == y = [x] ++ firstDuplicate (y:xs)
-    | otherwise = firstDuplicate (y:xs)
-
 
 isStraight :: Hand -> Maybe Hand
 isStraight [] = Nothing
@@ -82,3 +103,63 @@ isStraight hand
 isSucc xs = and $ zipWith (<=) xs (drop 1 xs)
 
 
+compareValue :: Card -> Card -> Ordering
+compareValue (a,_) (b,_) = compare a b
+
+cardEqual :: Card -> Card -> Bool
+cardEqual (a,_) (b,_) = a == b
+
+
+firstDuplicate :: Hand -> Hand
+firstDuplicate xs = case duplicates  xs of
+                      [] -> []
+                      xs -> head xs
+
+duplicates :: Hand -> [Hand]
+duplicates [] = []
+duplicates (x:xs)
+    | null dup = duplicates xs
+    | otherwise = (x:dup) : (duplicates rem)
+    where sep = List.span (cardEqual x) xs
+          dup = fst sep
+          rem = snd sep
+
+
+isFourKind :: Hand -> Maybe Hand
+isFourKind = isNKind 4
+
+isThreeKind :: Hand -> Maybe Hand
+isThreeKind = isNKind 3
+
+isPair :: Hand -> Maybe Hand
+isPair = isNKind 2
+
+isNKind :: Int -> Hand -> Maybe Hand
+isNKind n hand
+    | (length duplicate) == n = Just duplicate
+    | otherwise = Nothing
+    where duplicate = firstDuplicate hand
+
+hasDupOrder :: Int -> Int -> [[Card]] -> Bool
+hasDupOrder x y list = and $ zipWith ($) [(hasLength x), (hasLength y)] list
+    where hasLength n = (==n) . length
+
+isFullHouse :: Hand -> Maybe Hand
+isFullHouse hand
+    | null list = Nothing
+    | hasOrder 2 3 = Just ((last list) ++ (head list))
+    | hasOrder 3 2 = Just hand
+    | otherwise = Nothing
+    where list = duplicates hand
+          hasOrder x y = hasDupOrder x y list
+
+isTwoPair :: Hand -> Maybe Hand
+isTwoPair hand
+    | null list = Nothing
+    | hasOrder 2 2 = Just (concat list)
+    | otherwise = Nothing
+    where list = duplicates hand
+          hasOrder x y = hasDupOrder x y list
+
+highCard :: Hand -> Card
+highCard = head
